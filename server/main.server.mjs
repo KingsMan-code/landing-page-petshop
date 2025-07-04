c4bd22b88432e7b3bfbcabf00f138e855bbafdc2
Lucas Figueiredo <lucasferreirafigueiredo@outlook.com>
2025-07-04 01:11:18 -0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ANDBY4HD.mjs";import"./chunk-AIMMXAZ4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-PI5FAHGM.mjs";import"./chunk-AIMMXAZ4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
